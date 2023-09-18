cacd611134ac459167c73ba946d878e6a9c61f52
nadeshda18 <nadeshda02@hotmail.com>
2023-09-18 22:05:28 +0000

    added prototypes and library

--- sort.h
+++ sort.h
@@ -1,6 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,5 +23,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int lomuto_partition(int *array, int start, int end, int size);
+void quick_sort_helper(int *array, int low, int high, size_t size);
 
 #endif
